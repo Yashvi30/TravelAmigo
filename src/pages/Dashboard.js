cf8908dcd983af07722baccece7a55526ecccb43
Rohit Kharsan <rkharsan64@gmail.com>
2021-10-29 23:28:12 +0530

    Add basic authentication using Google

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,17 @@
+import { signOut } from "@firebase/auth";
+import { useAuth, useUser } from "reactfire";
+
 const Dashboard = () => {
+  const auth = useAuth();
+  const { status, data: user } = useUser();
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <h2>{status}</h2>
+
+      {status === "success" && <p>{JSON.stringify(user)}</p>}
+      <button onClick={() => signOut(auth)}>Sign out!</button>
     </div>
   );
 };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import { useAuth } from "reactfire";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
 const Login = () => {
+  const auth = useAuth();
+  const provider = new GoogleAuthProvider();
+  const [error, setError] = useState(null);
+
+  const signIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        setError(null);
+      })
+      .catch((err) => {
+        console.log(JSON.stringify(err));
+        setError(err);
+      });
+  };
+
   return (
     <div>
       <h1>Login to TravelAmigo!</h1>
+      <button onClick={signIn}>Login with Google</button>
+      {error && <p>{JSON.stringify(error)}</p>}
     </div>
   );
 };
