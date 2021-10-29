79316b734ad668a5643f78f313cc95aec2c2c919
Rohit Kharsan <rkharsan64@gmail.com>
2021-10-29 23:59:16 +0530

    Add authentication using email and password

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import { useAuth } from "reactfire";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const Login = () => {
-  const auth = useAuth();
-  const provider = new GoogleAuthProvider();
   const [error, setError] = useState(null);
 
-  const signIn = () => {
-    signInWithPopup(auth, provider)
-      .then((result) => {
+  const auth = useAuth();
+  const googleProvider = new GoogleAuthProvider();
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(() => {
         setError(null);
       })
       .catch((err) => {
@@ -18,11 +23,58 @@ const Login = () => {
       });
   };
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const emailChangeHandler = (e) => setEmail(e.target.value);
+  const passwordChangeHandler = (e) => setPassword(e.target.value);
+
+  const signInWithEmail = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        setError(null);
+      })
+      .catch((err) => {
+        console.log(JSON.stringify(err));
+        setError(err);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    signInWithEmail();
+
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div>
       <h1>Login to TravelAmigo!</h1>
-      <button onClick={signIn}>Login with Google</button>
-      {error && <p>{JSON.stringify(error)}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email</label>
+          <br />
+          <input type="email" value={email} onChange={emailChangeHandler} />
+        </div>
+        <div>
+          <label>Password</label>
+          <br />
+          <input
+            type="password"
+            value={password}
+            onChange={passwordChangeHandler}
+          />
+        </div>
+        <div>
+          <button type="submit">Log in</button>
+          <button type="reset">Clear</button>
+        </div>
+      </form>
+      <hr />
+      <button onClick={signInWithGoogle}>Login with Google</button>
+      {error && <p>{error.code}</p>}
     </div>
   );
 };
