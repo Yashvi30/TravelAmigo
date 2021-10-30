807e2af3ecd823139aacd9067d176b25bf7254ef
Rohit Kharsan <rkharsan64@gmail.com>
2021-10-30 20:56:07 +0530

    Add Loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,14 @@
 const Loader = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-400 flex justify-center items-center">
+      <div className="bg-white p-16 rounded-xl shadow-lg cursor-wait select-none">
+        <div className="text-5xl font-bold text-gray-700 flex flex-col md:flex-row gap-8 items-center">
+          <div className="animate-spin w-16 h-16 rounded-full bg-gradient-to-r from-yellow-500 via-red-500 to-pink-500 "></div>
+          <div>Loading</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
