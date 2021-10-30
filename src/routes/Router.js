3902228f824c331d1cd0f4546b30ed2480f0052d
Rohit Kharsan <rkharsan64@gmail.com>
2021-10-31 00:15:20 +0530

    Fix router

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -14,7 +14,7 @@ import Chat from "../components/Chat";
 const Router = () => {
   const { status, data: signedIn } = useSigninCheck();
 
-  if (status === "loading" || true) {
+  if (status === "loading") {
     return <Loader />;
   }
 
