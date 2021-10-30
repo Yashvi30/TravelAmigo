749eb4a182be98439ed766643f1a0fc5fb9139dc
Yashvi Garg <yashvigarg330@gmail.com>
2021-10-31 00:10:59 +0530

    loader added

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -12,14 +12,21 @@
 // };
 
 // export default Loader;
+import React from "react";
+import "../styles/Loader.scss";
+
 const Loader = () => {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-400 flex justify-center items-center">
-      <div className="bg-white p-8 rounded-xl shadow-lg cursor-wait select-none">
-        <div className="text-2xl font-bold text-gray-700 flex flex-col md:flex-row gap-4 items-center">
-          <div className="animate-spin w-8 h-8 rounded-full bg-gradient-to-r from-yellow-500 via-red-500 to-pink-500 "></div>
-          <div>Loading</div>
-        </div>
+    <div class="example1">
+      <div class="block1">
+        <div class="item1"></div>
+        <div class="item1"></div>
+        <div class="item1"></div>
+        <div class="item1"></div>
+        <div class="item1"></div>
+        <div class="item1"></div>
+        <div class="item1"></div>
+        <div class="item1"></div>
       </div>
     </div>
   );
